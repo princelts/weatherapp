01c61c8853c571626928daf48a0d684ce5b3a9db
d304219 <d304219@edu.curio.nl>
2025-02-03 00:42:51 +0100

    added checkweather function

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const apiKey = "933cc632626a501506be3dfd6e26f221";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=rotterdam";
 
+async function checkWeather() {
+    const response = await fetch(apiUrl + `&appid=${apiKey}`)
+    var data = await response.json();
+
+    console.log(data)
+}
+
+checkWeather();
