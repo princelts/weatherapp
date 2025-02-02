222d50073f171bf1f4f859588512c442e06a5162
d304219 <d304219@edu.curio.nl>
2025-02-03 00:58:17 +0100

    added variables

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const apiKey = "933cc632626a501506be3dfd6e26f221";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=rotterdam";
 
-async function checkWeather() {
-    const response = await fetch(apiUrl + `&appid=${apiKey}`)
-    var data = await response.json();
+let cityName = document.querySelector(".city");
+let temp = document.querySelector(".temp");
+let humidity = document.querySelector(".humidity");
+let wind = document.querySelector(".wind");
+
 
     console.log(data)
 }
